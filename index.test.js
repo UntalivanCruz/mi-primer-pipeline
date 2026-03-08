be654b12eac56abaedc0b2be3211c28fe95ca922
Ivan Cruz <25679384+UntalivanCruz@users.noreply.github.com>
2026-03-07 18:36:13 -0600

    fix: test fail

--- index.test.js
+++ index.test.js
@@ -5,6 +5,6 @@ test('2 + 3 = 5', () => {
   expect(sum(2, 3)).toBe(5);
 });
 
-test('10 + 8 = 25', () => {
-  expect(sum(10, 8)).toBe(25);
+test('10 + 15 = 25', () => {
+  expect(sum(10, 15)).toBe(25);
 });
